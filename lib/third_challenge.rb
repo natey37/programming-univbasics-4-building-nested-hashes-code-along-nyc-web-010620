d470f0428801c8941cf5d71174dd324bc196d6be
Nathan Brickett <natey37@gmail.com>
2019-12-07 20:44:46 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -39,16 +39,7 @@ def third_challenge
         :status => "alive"
         },
       :heroine_friends => [
-        {
-          :name => "Steven", 
-          :age => "30", 
-          :attitude => "confused"
-        },
-        {
-          :name => "Nurse", 
-          :age => "44", 
-          :attitude => "worried"
-        }
+        
         ]
    }
   }
