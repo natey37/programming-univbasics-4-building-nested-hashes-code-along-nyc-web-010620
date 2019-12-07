29b63e1868b8864201b5bc866650d9d8437da773
Nathan Brickett <natey37@gmail.com>
2019-12-07 20:34:07 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -27,12 +27,14 @@ def third_challenge
         },
       :matriarch => {
         #your key/value pairs here
-        :name => "Juliet", 
-        :age => "15", 
-        :status => "alive"
+        :name => "Lady Capulet", 
+        :age => "51", 
         },
       :heroine => {
         #your key/value pairs here
+        :name => "Juliet", 
+        :age => "15", 
+        :status => "alive"
         },
       :heroine_friends => []
    }
